aed47485a327871fb2cecf5b9fbdd939541b51fd
curti-s <matthewscurtis81@gmail.com>
2018-03-14 08:32:25 +0300

    Declared CrimeListActivity as the MAIN and LAUNCHER in the manifest.xml file

---
+++ app/src/main/java/matthews/curtis/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package matthews.curtis.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by curti.s_ on 3/14/2018.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
