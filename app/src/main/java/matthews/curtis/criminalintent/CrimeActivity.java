608900b8c7dc095e6eff9e5b60fe3dd0ea0aa654
curti-s <matthewscurtis81@gmail.com>
2018-03-14 08:20:48 +0300

    Added an abstract class (SingleFramentActivity) to be used by activity classes

--- app/src/main/java/matthews/curtis/criminalintent/CrimeActivity.java
+++ app/src/main/java/matthews/curtis/criminalintent/CrimeActivity.java
@@ -5,21 +5,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
     // AppCompatActivity is a subclass of FragmentActivity which is how I was able to use support
     // fragments in this chapter.
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        // Adding the CrimeFragment
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if(fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
+
 }
