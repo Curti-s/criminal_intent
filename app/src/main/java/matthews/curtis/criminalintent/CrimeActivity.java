48ff7db526a1d24da5c694aea6986d19c9a7f95f
curti-s <matthewscurtis81@gmail.com>
2018-03-14 08:02:26 +0300

    Adding the generic app/src/main/res/layout/activity_fragment.xml, modified the layout to be inflated in CrimeActivity

--- app/src/main/java/matthews/curtis/criminalintent/CrimeActivity.java
+++ app/src/main/java/matthews/curtis/criminalintent/CrimeActivity.java
@@ -12,7 +12,7 @@ public class CrimeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         // Adding the CrimeFragment
         FragmentManager fm = getSupportFragmentManager();
